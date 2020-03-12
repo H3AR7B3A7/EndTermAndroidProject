c5623c2cd0ee31fb93ebcbced7a459345b5b26d3
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-03-12 21:06:40 +0100

    Initial commit

---
+++ app/src/main/java/be/SabahLeanderSteven/endtermandroidproject/MainActivity.java
@@ -0,0 +1,14 @@
+package be.SabahLeanderSteven.endtermandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
