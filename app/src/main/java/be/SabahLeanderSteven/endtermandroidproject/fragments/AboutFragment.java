80884d3b1b33d512aa93f2a3552188b99b8ce65c
leonaire87 <leander_catteeuw@hotmail.com>
2020-04-13 16:42:58 +0200

    added the history on aboutpage

---
+++ app/src/main/java/be/SabahLeanderSteven/endtermandroidproject/fragments/AboutFragment.java
@@ -0,0 +1,28 @@
+package be.SabahLeanderSteven.endtermandroidproject.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import be.SabahLeanderSteven.endtermandroidproject.R;
+
+public class AboutFragment extends Fragment {
+
+
+
+    public AboutFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
